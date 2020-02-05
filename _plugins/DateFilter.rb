f0652165d32b8236d288af9420e21b0de10e6601
Anna Prigkipaki <apgr1976@gmail.com>
2020-02-05 14:47:12 +0200

    Create DateFilter.rb

---
+++ _plugins/DateFilter.rb
@@ -0,0 +1,9 @@
+module DateFilter
+  MONTHS = %w(Ιανουαρίου Φεβρουαρίου Μαρτίου Απριλίου Μαϊου Ιουνίου Ιουλίου Αυγούστου Σεπτεμβρίου Οκτωβρίου Νοεμβρίου Δεκεμβρίου)
+
+  def greek_long_month(input)
+    MONTHS[input.strftime("%m").to_i - 1]
+  end
+end
+
+Liquid::Template.register_filter(DateFilter)
