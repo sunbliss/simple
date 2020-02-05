3d80681b60091d61f5968206de381c70cd018d1e
Anna Prigkipaki <apgr1976@gmail.com>
2020-02-05 15:15:23 +0200

    Update DateFilter.rb

--- _plugins/DateFilter.rb
+++ _plugins/DateFilter.rb
@@ -1,8 +1,10 @@
+module Jekyll
 module DateFilter
   MONTHS = %w(Ιανουαρίου Φεβρουαρίου Μαρτίου Απριλίου Μαϊου Ιουνίου Ιουλίου Αυγούστου Σεπτεμβρίου Οκτωβρίου Νοεμβρίου Δεκεμβρίου)
 
   def greek_long_month(input)
     MONTHS[input.strftime("%m").to_i - 1]
+    end
   end
 end
 
